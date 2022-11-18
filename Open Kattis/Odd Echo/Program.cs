953f6db79f55202a0bb27ee65b6a601eb0c501b8
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 09:53:12 +0000

    "Odd Echo" Challenge Created and Completed

---
+++ Open Kattis/Odd Echo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int numberOfWords = int.Parse(Console.ReadLine());
+            string[] words = new string[numberOfWords];
+
+            for (int i = 0; i < numberOfWords; i++)
+                {
+                  words[i] = Console.ReadLine();
+                }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if ((i % 2 == 0))
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+
+
+        }
+    }
+}
