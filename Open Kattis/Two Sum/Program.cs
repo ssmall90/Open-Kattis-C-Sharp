6a13456e025ff61fb63ba3d140c19439d445b493
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 10:08:30 +0000

    "Two Sum" Challenge Created and Completed

---
+++ Open Kattis/Two Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string numbers = Console.ReadLine();
+            string number1 = numbers.Split()[0];
+            string number2 = numbers.Split()[1];
+
+            int firstNumber = Convert.ToInt32(number1);
+            int secondNumber = Convert.ToInt32(number2);
+
+            Console.WriteLine(firstNumber + secondNumber);
+        }
+    }
+}
