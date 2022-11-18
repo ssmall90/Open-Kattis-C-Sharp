b209013f8bcb9b365b2fcb14cae852fdfba7a33a
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 09:16:22 +0000

    "Echo Echo Echo" Challenge Created and Completed

---
+++ Open Kattis/Echo Echo Echo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine($"{input} {input} {input}");
+        }
+    }
+}
