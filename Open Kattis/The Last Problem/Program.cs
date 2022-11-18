f10568a721b18919842e11ec229505ac889c096a
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 10:10:27 +0000

    "The Last Problem" Challenge Created and Completed

---
+++ Open Kattis/The Last Problem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Thank you, {name}, and farewell!");
+        }
+    }
+}
