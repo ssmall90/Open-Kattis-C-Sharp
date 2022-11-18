70e2da0d0fc4015d7ea85be2a5b10111368b1023
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 09:59:33 +0000

    "Which Is Greater" Challenge Created and Completed

---
+++ Open Kattis/Which Is Greater/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string numbers = Console.ReadLine();
+            string firstNumber = numbers.Split(" ")[0];
+            string secondNumber = numbers.Split(" ")[1];
+            int firstNumber1 = Convert.ToInt32(firstNumber);
+            int secondNumber1 = Convert.ToInt32(secondNumber);
+            if (firstNumber1 > secondNumber1)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
