40e5c8c2143d300bce17c96f00f708170a31a6ef
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 10:19:12 +0000

    "Area Of Triangle" Challenge Created and Completed

---
+++ Open Kattis/Triangle Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string input = (Console.ReadLine());
+            string baseOfTriangle = input.Split(" ")[0];
+            string heightOfTriangle = input.Split(" ")[1];
+
+            int baseOfTriangle1 = Convert.ToInt32(baseOfTriangle);
+            int heightOfTriangle1 = Convert.ToInt32(heightOfTriangle);
+
+            float areaOfTriangle = ((baseOfTriangle1 * heightOfTriangle1) / 2f);
+
+            Console.WriteLine(areaOfTriangle);
+
+        }
+    }
+}
