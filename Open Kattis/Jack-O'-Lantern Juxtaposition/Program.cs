41dbec53231834d4767af677f4d719264d4dcce5
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 09:10:54 +0000

    "Jack-O-Lantern Juxtaposition" Challenege Created and Completed

---
+++ Open Kattis/Jack-O'-Lantern Juxtaposition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string numberOfEyeDesigns = input.Split(" ")[0]; 
+            string numberOfNoseDesigns = input.Split(" ")[1];
+            string numberOfMouthDesigns = input.Split(" ")[2];
+            int numberOfEyeDesigns1 = Convert.ToInt32(numberOfEyeDesigns);
+            int numberOfNoseDesigns1 = Convert.ToInt32(numberOfNoseDesigns);
+            int numberOfMouthDesigns1 = Convert.ToInt32(numberOfMouthDesigns);
+            Console.WriteLine(numberOfEyeDesigns1 * numberOfNoseDesigns1 * numberOfMouthDesigns1);
+        }
+    } 
+}
