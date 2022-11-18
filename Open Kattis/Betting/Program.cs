946b8420247b53591aa450d57114bd789b5f7547
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 09:32:37 +0000

    "Betting" Challenge Created and Completed

---
+++ Open Kattis/Betting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int ratio1 = int.Parse(Console.ReadLine());
+            int ratio2 = 100 - ratio1;
+
+            Console.WriteLine(100f / ratio1);
+            Console.WriteLine(100f / ratio2);
+
+        }
+    }
+}
