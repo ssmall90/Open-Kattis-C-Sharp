6c76c205e815a6b98244a0c072e486ed23f7ed6a
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-18 09:39:52 +0000

    "Autori" Challenge Created and Completed

---
+++ Open Kattis/Autori/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine(); 
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsUpper(input[i]))
+                {
+                    Console.Write(input[i]);
+                }
+            }
+
+        }
+    }
+}
