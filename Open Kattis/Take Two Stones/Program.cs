e7ab24b883a83710ba3f1f5f22a3c5f7e93d6184
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-20 12:27:26 +0000

    "Take Two Stones" Challenge Created and Completed

---
+++ Open Kattis/Take Two Stones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int numberOfStones = int.Parse(Console.ReadLine());
+            int remainingStones = numberOfStones;
+
+
+            for (int i = 0; i < numberOfStones; i += 2)
+            {
+                remainingStones = remainingStones - 2;
+                if (remainingStones <=1)
+                {
+                    break;
+                }
+            }
+
+            if (remainingStones % 2 == 0)
+            {
+                Console.WriteLine("Bob");
+            }
+            else if (!(remainingStones % 2 == 0))
+            {
+                Console.WriteLine("Alice");
+            }
+
+        }
+    }
+}
