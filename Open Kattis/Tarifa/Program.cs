36196d595b5d52c883797d97f65127eb66da18f7
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-20 11:55:14 +0000

    "Tarifa" Challenge Created and Completed

---
+++ Open Kattis/Tarifa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int monthlyAllowance = int.Parse(Console.ReadLine());
+            int numberOfMonths = int.Parse(Console.ReadLine()); 
+            int[] arrayOfUsage = new int[numberOfMonths];
+            int totalRemaining = monthlyAllowance;
+
+            for (int i = 0; i < numberOfMonths; i++)
+            {
+                arrayOfUsage[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arrayOfUsage.Length; i++)
+            {
+                totalRemaining -= arrayOfUsage[i];
+                totalRemaining += monthlyAllowance;
+            }
+
+            Console.WriteLine(totalRemaining);
+        }
+    }
+}
