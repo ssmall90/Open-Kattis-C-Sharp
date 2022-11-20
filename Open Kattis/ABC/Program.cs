8da9697b122e337809e79f5c976273c0a2d807a4
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-20 13:21:05 +0000

    "ABC" Challenge Created and Completed

---
+++ Open Kattis/ABC/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string numbers = Console.ReadLine();
+            string abc = Console.ReadLine();
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            
+
+            List<string> numberlist = numbers.Split(" ").ToList();
+            List<int> intNumberList = numberlist.Select(int.Parse).ToList();
+
+
+            intNumberList.Sort();
+
+            foreach(char letter in abc)
+            {
+                if (letter == 'A')
+                {
+                    a = intNumberList[0];
+                    Console.Write(a + " ");
+                }
+                else if (letter == 'B')
+                {
+                    b = intNumberList[1];
+                    Console.Write(b + " ");
+                }
+                else if (letter == 'C')
+                {
+                    c = intNumberList[2];
+                    Console.Write(c + " ");
+                }
+            }
+
+
+        }
+    }
+}
