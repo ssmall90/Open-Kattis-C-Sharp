cee62aaea0e7e4bbfc37b2ee147ecda2f89094fe
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-21 18:31:32 +0000

    "Building Pyramids" Challenge Created and Completed

---
+++ Open Kattis/Building Pyramids/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace myNameSpace
+{
+    public class myProgram
+    {
+        static void Main()
+        {
+            int blocksRemaining = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i < blocksRemaining; i += 2)
+            {
+                blocksRemaining -= (i * i);
+                count++;
+                if (blocksRemaining - (i + 2) * (i + 2) < 0)
+                {
+                    goto end;
+                }
+            }
+        end:;
+            Console.WriteLine(count);
+        }
+    }
+}
