dab3a2836ce459aff62d715f1b3bdff8bebff254
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-11-23 11:36:39 +0000

    "Sort Two Numbers" Challenege Created and Completed

---
+++ Open Kattis/Sort Two Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace MmyNameSpace
+{
+    class myProgram
+    {
+        static void Main ()
+        {
+            string input = Console.ReadLine ();
+            string firstNumber = input.Split(" ")[0];
+            string secondNumber = input.Split(" ")[1];
+            int firstNumber1 = Convert.ToInt32(firstNumber);
+            int secondNumber1 = Convert.ToInt32(secondNumber);
+
+            if (firstNumber1 < secondNumber1)
+            {
+                Console.WriteLine($"{firstNumber1} {secondNumber1}");
+            }
+            else if (secondNumber1 < firstNumber1)
+            {
+                Console.WriteLine($"{secondNumber1} {firstNumber1}");
+            }
+            else
+            {
+                Console.WriteLine($"{firstNumber1} {secondNumber1}");
+            }
+        }
+    }
+}
