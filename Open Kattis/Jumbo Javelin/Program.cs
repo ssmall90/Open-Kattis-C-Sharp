c51ac2754e900cc3c62afb47c92d68d5c4624671
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-06 12:55:01 +0000

    "Jumbo Javelin" Challenge Created and Completed

---
+++ Open Kattis/Jumbo Javelin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main ()
+        {
+            string input = Console.ReadLine ();
+            int intInput = Convert.ToInt32 (input);
+            int[] inputArray = new int[intInput];
+            int total = 0;
+
+            for (int i = 0; i < intInput; i++)
+            {
+                inputArray[i] = int.Parse(Console.ReadLine()) ;
+                total += inputArray[i];
+            }
+            total -= (intInput -1); 
+
+            Console.WriteLine(total);
+
+        }
+    }
+}
