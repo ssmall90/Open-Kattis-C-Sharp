825ed18ee6340438bb55fe9cc485457b39065cea
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-06 11:29:14 +0000

    "Stuck In A Time Loop" Challenege Created and Completed

---
+++ Open Kattis/Stuck In A Time Loop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main ()
+        {
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                Console.WriteLine(i + " " + "Abracadabra");
+            }
+        }
+    }
+}
