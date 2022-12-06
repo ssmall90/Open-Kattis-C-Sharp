4530cb344ea9f31e32ce1d1644abc2245760e902
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-06 12:45:15 +0000

    "Quadrant Selection" Challenge Created and Completed

---
+++ Open Kattis/Quadrant Selection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main ()
+        {
+            int intInput1 = int.Parse(Console.ReadLine());  
+            int intInput2 = int.Parse(Console.ReadLine());
+
+
+            if (intInput1 > 0 && intInput2 > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (intInput1 < 0 && intInput2 < 0)
+            {
+                Console.WriteLine("3");
+            }
+            else if (intInput1 > 0 && intInput2 < 0)
+            {
+                Console.WriteLine("4");
+            }
+            else if (intInput1 < 0 && intInput2 > 0)
+            {
+                Console.WriteLine("2");
+            }
+        }
+    }
+}
+
