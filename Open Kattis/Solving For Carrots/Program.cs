0b314fc364b8ea2fed5a7c267a0e44ce87dd677e
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-06 11:50:26 +0000

    "Solving For Carrots" Challenege Created and Completed

---
+++ Open Kattis/Solving For Carrots/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string input1 = input.Split(" ")[0];
+            string input2 = input.Split(" ")[1];
+            int numberOfDescription = Convert.ToInt32(input1);
+            int numberOfCarrots = Convert.ToInt32(input2);
+
+            for (int i = 0; i < numberOfDescription; i++)
+            {
+                Console.ReadLine();
+            }
+
+            Console.WriteLine(numberOfCarrots);
+
+
+
+
+
+        }
+    }
+}
