25c19101ed83ad9e04155a48c8a7d204764e4b5f
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-07 13:07:48 +0000

    "Greetings" Challenge Created and Completed

---
+++ Open Kattis/Greetings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            List<char> outputList = new List<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                outputList.Add(input[i]);
+
+                if (input[i] == 'e')
+                {
+                    outputList[i] = 'e';
+                    outputList.Add('e');
+                }
+            }
+            foreach(char c in outputList)
+            {
+                Console.Write(c);
+            }
+
+        }
+    }
+}
