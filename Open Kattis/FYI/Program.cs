3e4e1d08375091869e6213aca6e0521cf5c2c1b3
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-07 14:30:35 +0000

    "FYI" Challenge Created and Completed

---
+++ Open Kattis/FYI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace myNAmeSpace
+{
+    class myProgram
+    {
+        static void Main ()
+        {
+            int count = 0;
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i < 3)
+                {
+                    if (input[i] == '5')
+                    {
+                        count++;
+                    }
+                    else if (!(input[i] == '5'))
+                    {
+                        count--;
+                    }
+                }
+
+            }
+
+            if (count == 3)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
