82c52d5adca3c00eb0b0bfa4aac155dc2325a15d
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-07 12:49:02 +0000

    "Knight Packing" Challenge Created and Completed

---
+++ Open Kattis/Knight Packing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("second");
+            }
+            else
+            {
+                Console.WriteLine("first");
+            }
+        }
+    }
+}
