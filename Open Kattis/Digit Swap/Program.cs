0635ff987d9e0e64750cf3778584cf3ff8be41c2
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-13 12:20:04 +0000

    "Digit Swap" Challenge Created and Completed

---
+++ Open Kattis/Digit Swap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string safeCode = Console.ReadLine();
+            string lastDigit = safeCode.Substring(safeCode.Length - 1, 1);
+            string firstDigit = safeCode.Substring(0,1);
+            string reversedSafeCode = lastDigit + firstDigit;
+            int output = int.Parse(reversedSafeCode);
+            Console.WriteLine(output);
+        }
+    }
+}
