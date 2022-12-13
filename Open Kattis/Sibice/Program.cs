526f5b8d9070241ae0cc31bb9cc26439b01556d2
ssmall90 <ssmall9595@icloud.com>
2022-12-13 17:59:40 +0000

    "Sibice" Challenge Created and Completed

---
+++ Open Kattis/Sibice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        double diagonalCalculator(ref float length, ref float width, out double output)
+        {
+           
+            return output = (length * length) + (width * width);
+
+        }
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string numberOfMatches = input.Split(" ")[0];
+            string widthOfBoxString = input.Split(" ")[1];
+            string lengthOfBoxString = input.Split(" ")[2];
+            float widthOfBox = float.Parse(widthOfBoxString);
+            float lengthOfBox = float.Parse(lengthOfBoxString);
+
+            double finalOutput;
+            double diagonalOutput = 0 ;
+
+            float[] lengthOfMatches = new float[int.Parse(numberOfMatches)];
+
+            for (int i = 0; i < lengthOfMatches.Length; i++)
+            {
+                lengthOfMatches[i] = float.Parse(Console.ReadLine());
+            }
+
+            var myClass = new myProgram();
+
+            myClass.diagonalCalculator(ref widthOfBox, ref lengthOfBox, out diagonalOutput);
+
+            finalOutput = Math.Sqrt(diagonalOutput);
+
+
+            for (int i = 0; i < lengthOfMatches.Length; i++)
+            {
+                if (lengthOfMatches[i] > finalOutput)
+                {
+                    Console.WriteLine("NE");
+                }
+                else if (lengthOfMatches[i] <= finalOutput)
+                {
+                    Console.WriteLine("DA");
+                }
+            }
+        }
+    }
+}
