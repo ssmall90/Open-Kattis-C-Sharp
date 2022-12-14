d08365bba6e3a6d3d7b5ee851f36055efb567bf6
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-14 16:54:36 +0000

    "Shattered Cake" Challenge Created and Completed

---
+++ Open Kattis/Shattered Cake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int widthOfCake = int.Parse(Console.ReadLine());
+            int numberOfPieces = int.Parse(Console.ReadLine());
+            int area = 0;
+            for (int i = 0; i < numberOfPieces; i++)
+            {
+                string [] sizeOfPieces = Console.ReadLine().Split(" ");
+                area += Convert.ToInt32(sizeOfPieces[0]) * Convert.ToInt32(sizeOfPieces[1]);
+            }
+            Console.WriteLine(area / widthOfCake);
+        }    
+    }
+}
