3ea00889f50089b2d93787765b18e139f66dfd96
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2022-12-14 17:17:50 +0000

    "Rating Problems" Challenge Created and Completed

---
+++ Open Kattis/Rating Problems/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    public class myProgram
+    {
+
+        public void findAverage(ref int pRemaining, out float pMaxAverage, out float pMinAverage )
+        {
+            
+            pMaxAverage = pRemaining * 3;
+            pMinAverage = pRemaining * -3;
+            
+        }
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int numberOfJudges = int.Parse(input[0]);
+            int judgesThatHaveVoted = int.Parse(input[1]);
+            int totalVote = 0;
+            int remaingingVotes = numberOfJudges - judgesThatHaveVoted;
+            float lowestAverage;
+            float highestAverage;
+
+            for (int i = 0; i < judgesThatHaveVoted; i++)
+            {
+                totalVote += int.Parse(Console.ReadLine());
+            }
+
+            var myClass = new myProgram();
+            myClass.findAverage(ref remaingingVotes, out highestAverage, out lowestAverage );
+
+            Console.WriteLine(((lowestAverage + totalVote) / numberOfJudges) + " " + ((highestAverage + totalVote) / numberOfJudges)) ;
+        }
+    }
+}
