b405e73f087be321671318522760645aa8d04187
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-01-12 11:52:52 +0000

    "Finding An A" Challenge Created and Completed

---
+++ Open Kattis/Finding An A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int subStart = 0; 
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a')
+                {
+                    subStart = i;
+                    break;
+                }
+            }
+
+
+            string output = input.Substring(subStart);
+
+            Console.WriteLine(output);
+        }
+    }
+}
