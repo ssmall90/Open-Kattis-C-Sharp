6b0c46a18ac0c1790b1d0968de9fd92fd0bba8c6
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-01-12 11:58:55 +0000

    "N-Sum" Challenge Created and Completed

---
+++ Open Kattis/N-Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int output = 0;
+
+            int[] numbersToAdd = input.Split(' ').Select(int.Parse).ToArray();
+
+            foreach(int number in numbersToAdd)
+            {
+                output += number;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
