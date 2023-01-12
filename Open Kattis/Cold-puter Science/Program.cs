25be00ecd8d298e927588cccb862c4a38fb67d00
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-01-12 11:34:40 +0000

    Cold-Puter Science Challenge Created and Completed

---
+++ Open Kattis/Cold-puter Science/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int count = 0; 
+
+            int numberOfTemps = int.Parse(Console.ReadLine());
+            string inputTemps = Console.ReadLine();
+
+            int[] intTemperatures = inputTemps.Split(' ').Select(int.Parse).ToArray();
+
+            foreach(int number in intTemperatures)
+            {
+                if (number < 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
