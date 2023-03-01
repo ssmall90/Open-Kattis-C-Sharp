147eb3ff5be0cdef9f3876c87a84ab9c1ac72593
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-03-01 16:49:14 +0000

    Metronome Challenge Created and Completed

---
+++ Open Kattis/Metronome/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml;
+
+namespace myNameSpace
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            double output = input / 4.0; 
+            Console.WriteLine(output);
+        }
+        
+    }
+} 
