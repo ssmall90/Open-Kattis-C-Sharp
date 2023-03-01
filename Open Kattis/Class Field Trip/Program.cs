7871237e4dd3892914ed28c9f22ab0fef0ffb2c2
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-03-01 19:40:44 +0000

    Class Field Trip Challenege Created and Completed

---
+++ Open Kattis/Class Field Trip/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace myNameSpace
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            string jointInput = input1 + input2;
+
+            List<char> joinedList = new List<char>();
+
+            for (int i = 0; i < jointInput.Length; i++)
+            {
+                joinedList.Add(jointInput[i]);
+            }
+            
+            joinedList.Sort();
+
+            string output = string.Empty;
+
+            for (int i = 0; i < joinedList.Count;i++)
+            {
+                output += joinedList[i];
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
