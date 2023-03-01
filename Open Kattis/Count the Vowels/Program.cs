4329a07ee815288d0201ed88d4dfaca4cd0d84a5
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-03-01 17:01:34 +0000

    Count the Vowels Challenege Created and Completed

---
+++ Open Kattis/Count the Vowels/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            string vowels = "aeiou";
+            int count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            { }
+                
+                if (vowels.Contains(input[i]))
+                {
+                    count++;
+                }
+                
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
