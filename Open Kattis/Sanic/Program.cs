ab83a982f56f99bc9e724106819da42f7713b0e0
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-13 11:25:22 +0100

    Sanic ChallengeCreated and Completed

---
+++ Open Kattis/Sanic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyNameSpace
+{
+    class MyProgram
+    {
+        public static void Main()
+        {
+            decimal userInput = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(userInput - 1);
+        }
+    }
+}
