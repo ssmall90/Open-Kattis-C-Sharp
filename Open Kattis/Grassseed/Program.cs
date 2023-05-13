fd9d22c5bd9715ee283c1e4344d6051d80f672a2
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-13 09:44:00 +0100

    Grass Seed Challenge Created and Completed

---
+++ Open Kattis/Grassseed/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        public static void Main()
+        {
+            decimal totalArea = 0;
+            string userInput = Console.ReadLine();
+            decimal costOfSeed = decimal.Parse(userInput);
+            string userInput2 = Console.ReadLine();
+            int numberOfLawns = int.Parse(userInput2);
+
+            for (int i = 0; i < numberOfLawns; i++)
+            {
+                string userInputLoop = Console.ReadLine();
+                string width = userInputLoop.Split()[0];
+                string length = userInputLoop.Split()[1];
+                decimal widthNum = decimal.Parse(width);
+                decimal lengthNum = decimal.Parse(length);
+                decimal total = widthNum * lengthNum;
+                totalArea += total;
+            }
+            decimal output = totalArea * costOfSeed;
+            Console.WriteLine(output);
+        }
+    }
+}
