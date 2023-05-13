08e2ffebd9fb6c7ac89f5117ff2901cba25e2930
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-13 10:25:35 +0100

    "A Real Challenge" Challenge Created and Completed

---
+++ Open Kattis/A Real Challenge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyNameSpace
+{
+    class MyProgram
+    {
+        public static void Main()
+        {
+            string userInput = Console.ReadLine();
+            decimal areaOfField = decimal.Parse(userInput);
+            decimal perimeter = (decimal)Math.Sqrt((double)areaOfField) * 4;
+            Console.WriteLine(perimeter);
+        }
+    }
+}
