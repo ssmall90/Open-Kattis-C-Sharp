f1b01f30215243495d17f3277d0395b17ad1de28
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-13 10:05:48 +0100

    Fizz Buzz Challenge Created and Completed

---
+++ Open Kattis/FizzBuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System; 
+
+namespace MyNameSpace
+{
+    class MyProgram
+    {
+        public static void Main()
+        {
+            string userInput = Console.ReadLine();
+            string fizz = userInput.Split()[0];
+            string buzz = userInput.Split()[1];
+            string numberOfLine = userInput.Split()[2];
+            int numberOfLines = int.Parse(numberOfLine);
+
+
+            int fizzNum = int.Parse(fizz);
+            int buzzNum = int.Parse(buzz);
+
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                if (i % fizzNum == 0)
+                {
+                    if (i % buzzNum == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (i % buzzNum == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
