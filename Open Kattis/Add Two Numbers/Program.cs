092cc8be1338f0e08d26b56245a16a17124ddb9f
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-15 14:07:09 +0100

    Add Two Numbers Challenge Created and Completed

---
+++ Open Kattis/Add Two Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyNamespace
+{
+    class program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(int.Parse(input.Split(" ")[0]) + int.Parse(input.Split(" ")[1])); 
+        }
+
+    }
+}
