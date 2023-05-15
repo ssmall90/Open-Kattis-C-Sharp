202edb83f43661b0ea5047a0ff0c912052e69801
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-15 14:20:07 +0100

    Batter Up Challenge Created and Completed

---
+++ Open Kattis/Batter Up/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace MyNamespace
+{
+    class program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string[] values = input2.Split(" ");
+            double total = 0;
+            int count = 0; 
+
+            foreach (string value in values)
+            {
+                if (int.Parse(value) > 0)
+                {
+                    total += int.Parse(value);
+                }
+                else if (int.Parse (value) < 0) 
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(total / (values.Length - count));
+        }
+
+    }
+}
