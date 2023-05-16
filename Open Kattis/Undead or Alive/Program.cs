33a27cc3627df50b86f9174e3d8e202f16763da3
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-16 10:52:11 +0100

    Undead or Alive Challenge Created and Completed

---
+++ Open Kattis/Undead or Alive/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countDead = 0;
+            int countAlive = 0;
+
+            string userInput = Console.ReadLine();
+            string[] data = userInput.Split(" ");
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].Contains(":)")) 
+                {
+                    countAlive++;
+                }
+                if (data[i].Contains(":("))
+                {
+                    countDead++;
+                }
+            }
+
+            if (countAlive == 0 && countDead == 0)
+            {
+                Console.WriteLine("machine");
+            }
+            else if(countAlive > 0 )
+            {
+                if (countDead == 0)
+                {
+                    Console.WriteLine("alive");
+                }
+                else
+                {
+                    Console.WriteLine("double agent");
+                }
+            }
+            else if (countDead > 0)
+            {
+                Console.WriteLine("undead");
+            }
+
+            
+        }
+    }
+}
