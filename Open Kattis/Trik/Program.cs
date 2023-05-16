77eecda446f02ac9156e20c57fc736219ef7ee4a
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-16 11:11:40 +0100

    Trik Challenge Created and Completed

---
+++ Open Kattis/Trik/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            char[] moves = userInput.ToCharArray();
+            int currentPosition = 1;
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+
+                switch (currentPosition)
+                {
+                    case 1: if (moves[i] == 'A') { currentPosition = 2; }
+                            else if (moves[i] == 'B') { currentPosition = 1; }
+                            else if (moves[i] == 'C') { currentPosition = 3; }
+                        break;
+                    case 2:
+                        if (moves[i] == 'A') { currentPosition = 1; }
+                        else if (moves[i] == 'B') { currentPosition = 3; }
+                        else if (moves[i] == 'C') { currentPosition = 2; }
+                        break;
+                    case 3:
+                        if (moves[i] == 'A') { currentPosition = 3; }
+                        else if (moves[i] == 'B') { currentPosition = 2; }
+                        else if (moves[i] == 'C') { currentPosition = 1; }
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(currentPosition);
+        }
+    }
+}
