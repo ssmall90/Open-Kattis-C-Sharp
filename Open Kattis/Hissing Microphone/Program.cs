dc09070af30c80e4f180d335d37f0978ca7829da
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-23 13:34:39 +0100

    Hissing Microphone Challenge Created and Completed

---
+++ Open Kattis/Hissing Microphone/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        string userInput = Console.ReadLine();
+        if (userInput.Contains("ss") || userInput.Contains("SS") || userInput.Contains("Ss") || userInput.Contains("sS"))
+        {
+            Console.WriteLine("hiss");
+        }
+        else
+        {
+            Console.WriteLine("no hiss");
+        }
+    }
+}
