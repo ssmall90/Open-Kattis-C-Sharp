1d5e4b86f3b77f38c59611698b1205dbdab90a41
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-23 13:51:03 +0100

    Spavanac Challenge Created and Completed

---
+++ Open Kattis/Spavanac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        string userInput = Console.ReadLine();
+        int hours = int.Parse(userInput.Split(" ")[0]);
+        int minutes = int.Parse(userInput.Split(" ")[1]);
+        if (minutes < 45)
+        {
+           if (hours > 0 ){ hours -= 1; minutes = (100 - 85) + minutes; }
+           else{hours = 23; minutes = (100 - 85) + minutes;}
+        }
+        else{minutes -= 45;}
+        Console.WriteLine(hours + " " + minutes);
+    }
+}
