b2271aba1391264d8036f962181ec7cad0c4cb6e
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-23 14:28:46 +0100

    Pokechat Challenge Created and Completed

---
+++ Open Kattis/Pokechat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string userTxtInput = Console.ReadLine();
+        string userIntInput = Console.ReadLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < userIntInput.Length; i++)
+        {
+            if (i % 3 == 0 && i > 0)
+            { sb.Append(' '); }
+            sb.Append(userIntInput[i]);
+        }
+        string[] ids = sb.ToString().Split(' ');
+        int[] intIds = ids.Select(int.Parse).ToArray();
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < intIds.Length; i++)
+        {
+            int temp = intIds[i];
+            output.Append(userTxtInput[temp - 1]);
+        }
+        Console.WriteLine(output.ToString());
+    }
+}
