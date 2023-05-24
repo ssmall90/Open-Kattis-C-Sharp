093b40fd1e539718b4d219a0b82d8cd13a4401d0
ssmall90 <ssmall9595@icloud.com>
2023-05-24 13:17:22 +0100

    Planina Challenge Created and Completed

---
+++ Open Kattis/Planina/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int userInput = int.Parse(Console.ReadLine());
+
+            int count = 1;
+            int squareRootValue = 3;
+            int temp = 0;
+
+            if (userInput > 1)
+            {
+                do
+                {
+                    temp = (squareRootValue * 2) - 1;
+                    squareRootValue = temp;
+                    count++;
+                }
+
+                while (count < userInput);
+                Console.WriteLine(squareRootValue * squareRootValue);
+            }
+            else
+            {
+                Console.WriteLine(squareRootValue * squareRootValue);
+            }
+
+        }
+    }
+}
