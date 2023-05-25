d6d58c00f0e7a073e7dd0ce5f75d9641208bb87f
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-25 14:35:11 +0100

    Last Factorial Digit Challenge Created and Completed

---
+++ Open Kattis/Last Factorial Digit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            string [] outputs = new string[numberOfInputs];
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                int currentValue = int.Parse(Console.ReadLine());
+                outputs[i] = Factorial(currentValue).ToString();
+            }
+
+            foreach (string val in outputs)
+            {
+                Console.WriteLine(val.Last());
+            }
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number-1);
+            
+        }
+    }
+}
