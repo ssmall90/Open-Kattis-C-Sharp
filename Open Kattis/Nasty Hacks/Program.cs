375a3cd98898e5fd32480ff77767c8886880ab6f
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-25 13:52:43 +0100

    Nasty Hacks Challenge Created and Completed

---
+++ Open Kattis/Nasty Hacks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int userInput = int.Parse(Console.ReadLine());
+            string[] values = new string[userInput];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                {
+
+                    if (int.Parse(values[i].Split(" ")[0]) > int.Parse(values[i].Split(" ")[1]) - int.Parse(values[i].Split(" ")[2]))
+                    {
+                        Console.WriteLine("do not advertise");
+                    }
+                    else if (int.Parse(values[i].Split(" ")[0]) < int.Parse(values[i].Split(" ")[1]) - int.Parse(values[i].Split(" ")[2]))
+                    {
+                        Console.WriteLine("advertise");
+                    }
+                    else { Console.WriteLine("does not matter"); }
+                }
+            }
+        }
+    }
+}
