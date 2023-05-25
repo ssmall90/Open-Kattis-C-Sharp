b5094c48397a160e9b31e355f037f82833b4e2cf
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-05-25 14:10:46 +0100

    Magic Trick Challenge Created and Completed

---
+++ Open Kattis/Magic Trick/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            int count = 0;
+
+            for (int i = 0; i < userInput.Length - 1; i++)
+            {
+
+                for (int j = i + 1; j < userInput.Length; j++)
+                {
+
+                    if (userInput[j] == userInput[i])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+
+                if (count > 0)
+                {
+                    break;
+                }
+
+            }
+            if (count > 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
