8bf7e69c9e8b8db370c6b2d22cb6e6e4bb572659
SHELDON SMALL <S.SMALL-2021@hull.ac.uk>
2023-06-06 11:43:43 +0100

    Howl Challenge Created and Completed

---
+++ Open Kattis/Howl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace MyNamespce
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string fenirHowl = Console.ReadLine();
+
+            StringBuilder howl = new StringBuilder();
+            howl.Append("AWH");
+
+            for (int i = 0; i < fenirHowl.Length; i++)
+            {
+                howl.Append("O");
+
+            }
+
+            Console.WriteLine(howl.ToString());
+        }
+    }
+}
