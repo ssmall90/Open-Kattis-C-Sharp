baaf2335bd37825368ff10bd4a7d865d968faa60
ssmall90 <ssmall9595@icloud.com>
2023-07-16 01:07:31 +0100

    Hangman Challenge Created and Completed

---
+++ Open Kattis/Hangman/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MyNameSpace
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string gameWord = Console.ReadLine();
+            string permutation = Console.ReadLine();
+
+            int count = 0;
+
+            char[] gameWordArr = gameWord.ToCharArray();
+            char[] guess = new char[gameWord.Length];
+
+
+
+            for (int i = 0; count < 10; i++)
+            {
+                char c = permutation[i];
+
+                if (gameWord.Contains(c))
+                {
+                    for (int j = 0; j < gameWord.Length; j++)
+                    {
+                        if (c == gameWordArr[j])
+                        {
+                            guess[j] = c;
+                        }
+
+                    }
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            string output = new string(guess);
+
+            bool hasWon = string.Equals(gameWord, output);
+
+            if (hasWon)
+            {
+                Console.WriteLine("WIN");
+            }
+            else
+            {
+                Console.WriteLine("LOSE");
+            }
+        }
+    }
+}
