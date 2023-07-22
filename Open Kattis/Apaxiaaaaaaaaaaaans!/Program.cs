32d604342534d735d96e631ec2d03baceb4cb8ed
ssmall90 <ssmall9595@icloud.com>
2023-07-22 09:57:47 +0100

    "Apaxiaaaaaaaaaaaans!" Challenge Created and Completed

---
+++ Open Kattis/Apaxiaaaaaaaaaaaans!/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyNamespace
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] chars = input.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] == chars[i+1])
+                {
+                    continue;
+                }
+                else
+                {
+                    sb.Append(chars[i]);
+                }
+            }
+
+            sb.Append(chars.Last());
+
+            Console.WriteLine(sb.ToString());
+
+
+        }
+    }
+}
