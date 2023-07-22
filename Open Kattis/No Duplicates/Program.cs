64c9e59a0995d1cf5933608a73c73fb7b416153c
ssmall90 <ssmall9595@icloud.com>
2023-07-22 09:11:22 +0100

    "No Duplicates" Challenge Created and Completed

---
+++ Open Kattis/No Duplicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(" ");
+
+            int count = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (words[i] == words[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count > 0)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine("yes");
+            }
+        }
+    }
+}
+
+
