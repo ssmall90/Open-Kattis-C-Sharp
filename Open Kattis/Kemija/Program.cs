9865efb5aca8d64186ea0f5af0ace904abf4b6d1
ssmall90 <ssmall9595@icloud.com>
2023-07-23 15:47:20 +0100

    Kemija Challenege Created and Completed

---
+++ Open Kattis/Kemija/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] sentenceSplit = input.Split(" ");
+
+            string[] outputSplit = new string[sentenceSplit.Length];
+            StringBuilder sb = new StringBuilder();
+
+            string consonants = ("bcdfghjklmnpqrstvwxyz");
+            string vowels = ("aeiou");
+            int breakpoint = 0;
+
+            foreach (string word in sentenceSplit)
+            {
+                int index = 0;
+                if(breakpoint > 0 && breakpoint < sentenceSplit.Length)
+                {
+                    sb.Append(" ");
+                }
+
+                while (index < word.Length)
+                {
+                    if (vowels.Contains(word[index])) 
+                    {
+                        sb.Append(word[index]);
+                        index += 3;
+                    }
+                    else if (consonants.Contains(word[index])) 
+                    { 
+                        sb.Append(word[index]);
+                        index++;
+                    }
+                           
+                }
+                breakpoint++;
+            }
+
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
