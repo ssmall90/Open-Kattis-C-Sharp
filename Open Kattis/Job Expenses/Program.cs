8c5fc9bfa6aed3d072fbcd694fa53cc5e6278d49
ssmall90 <ssmall9595@icloud.com>
2023-07-24 14:11:09 +0100

    "Job Expenses" Challenge Created and Completed

---
+++ Open Kattis/Job Expenses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace MyNamespce
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfEntries= int.Parse(Console.ReadLine());
+            string entriesString = Console.ReadLine();
+
+            string[] allValues = entriesString.Split();
+            int total = 0;
+
+            for(int i =0; i < allValues.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(allValues[i]))
+                {
+                    if (int.Parse(allValues[i]) < 0)
+                    {
+                        total += int.Parse(allValues[i]);
+                    }
+                }
+                continue;
+            }
+
+            if(total < 0)
+            {
+                string output = total.ToString().Substring(1);
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
