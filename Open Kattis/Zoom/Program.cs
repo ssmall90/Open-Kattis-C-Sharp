93ec4716d25b3ca8932e25c88530a3a4d288267a
ssmall90 <ssmall9595@icloud.com>
2023-08-03 13:31:49 +0100

    "Zoom" Challenge Created and Completed

---
+++ Open Kattis/Zoom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace MyNamespace;
+
+class Program
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        string values = Console.ReadLine();
+
+        string[] valuesStringArray = values.Split(" ");
+        int[] valuesIntArray = valuesStringArray.Select(int.Parse).ToArray(); 
+
+        int lengthOfList = int.Parse(input.Split(" ")[0]);
+        int zoomIndex = int.Parse(input.Split(" ")[1]);
+
+        int count = 1;
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < valuesIntArray.Length; i++)
+        {
+            if(count == zoomIndex)
+            {
+                sb.Append(valuesIntArray[i] + " ");
+                count = 1;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        sb.ToString().TrimEnd();
+        Console.WriteLine(sb);
+    }
+}
