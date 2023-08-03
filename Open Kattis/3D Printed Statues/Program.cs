c55c0771aa71aea7a884763a011fa78736b23943
ssmall90 <ssmall9595@icloud.com>
2023-08-03 13:12:25 +0100

    3D Printed Statues Challenge Created and Completed

---
+++ Open Kattis/3D Printed Statues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace MyNamespace;
+
+class Program
+{
+    public static void Main()
+    {
+        int totalStatues = int.Parse(Console.ReadLine());
+        int statuesPrinted = 0;
+        int printers = 1;
+        int days = 0;
+
+        while (statuesPrinted < totalStatues) 
+        {
+            if((totalStatues - statuesPrinted) > printers)
+            {
+                days += 1;
+                printers += printers;
+            }
+            else
+            {
+                days += 1;
+                statuesPrinted += printers;
+            }
+        }
+
+        Console.WriteLine(days);
+
+    }
+}
