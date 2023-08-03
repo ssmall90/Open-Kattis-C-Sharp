2c09beab93d0cb54f334c70282da6e6fcb0c8479
ssmall90 <ssmall9595@icloud.com>
2023-08-03 13:13:33 +0100

    99 Problems Cahllenge Deleted

--- Open Kattis/99 Problems/Program.cs
+++ Open Kattis/99 Problems/Program.cs
@@ -8,79 +8,35 @@ namespace myNameSpace
     {
         public static void Main()
         {
-            string input = Console.ReadLine();
-            char[] chars = input.ToCharArray();
-            int inputLength = input.Length;
-            int index = 0;
-            int indexTwo = 0;
-            StringBuilder sb = new StringBuilder();
-            bool firstOccurence = false;
-            int replacement = 0;
+            int input = int.Parse(Console.ReadLine());
 
-            if(int.Parse(input) > 149) 
+
+            static int GetClosestNumberEndingIn99(int inputNumber)
             {
-                index = inputLength - 2;
-                indexTwo =inputLength - 1;
+                int modResult = inputNumber % 100; // Get the last two digits of the input number
 
-                if ((char.GetNumericValue(chars[index]) == 4 && char.GetNumericValue(chars[indexTwo]) == 9) || char.GetNumericValue(chars[index]) > 4)
+                if (modResult == 99 || modResult >= 50) // If the last two digits are 99 or closer to 99, round up
                 {
-                    for (int i = 0; i < inputLength; i++)
-                    {
-                        if(i == index)
-                        {
-                            sb.Append('9') ;
-                        }
-                        else
-                        {
-                            sb.Append(input[i]);
-                        }
-                    }
-
-                    Console.WriteLine(sb.ToString().TrimStart('0'));
-
+                    Console.WriteLine( inputNumber + 100 - modResult);
+                    return inputNumber + 100 - modResult;
                 }
-                else
+                else // If the last two digits are closer to 0, round down
                 {
-                    for(int i =  inputLength-1 ; i >= 0; i--)
-                    {
-                        if (firstOccurence == false && i > inputLength - 3)
-                        {
-                            sb.Append('9');
-                        }
-                        else if(firstOccurence == false && char.GetNumericValue(chars[i]) == 0 && i > 0)
-                        {
-                            sb.Append('9');
-                        }
-                        else if (firstOccurence == false && char.GetNumericValue(chars[i]) > 0)
-                        {
-
-                            replacement = (int)char.GetNumericValue(chars[i]);
-                            sb.Append(replacement - 1);
-                            firstOccurence = true;
-
-                        }
-                        else
-                        {
-                            sb.Append(input[i]);
-                        }
-
-                    }
-                    char [] finalOutput = sb.ToString().ToCharArray();
-                    StringBuilder stringBuilder = new StringBuilder();
-
-                    for (int i = finalOutput.Length - 1; i >= 0; i--)
-                    {
-                        stringBuilder.Append(finalOutput[i]);
-                    }
-                    Console.WriteLine(stringBuilder.ToString().TrimStart('0'));
-
+                    Console.WriteLine( inputNumber - modResult);
+                    return inputNumber - modResult;
                 }
-
             }
-            else
+
+            if (input < 149)
             {
                 Console.WriteLine("99");
             }
+            else
+            {
+                GetClosestNumberEndingIn99(input);
+
+            }
+
         }
     }
 
