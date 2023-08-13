2ed1b9e7a64b5d6d92bb187ef15d42c876bec61b
ssmall90 <ssmall9595@icloud.com>
2023-08-13 11:41:07 +0100

    Simple Addition Challenge Created and Completed

---
+++ Open Kattis/Simple Addition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BigInteger val1 = BigInteger.Parse(Console.ReadLine());
+            BigInteger val2 = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine(val1 + val2);
+
+        }
+    }
+}
