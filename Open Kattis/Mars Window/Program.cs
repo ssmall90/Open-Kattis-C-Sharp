a4d971c86b7a9c711423d4254b8ffa90b1c7da40
ssmall90 <ssmall9595@icloud.com>
2023-10-06 10:15:50 +0100

    Mars Window Challenge Created and Completed

---
+++ Open Kattis/Mars Window/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class myProgram
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int a = input - 2017;
+
+            int b = a * 12 - 4; 
+
+            int c = b % 26; 
+
+            if (c < 12)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
