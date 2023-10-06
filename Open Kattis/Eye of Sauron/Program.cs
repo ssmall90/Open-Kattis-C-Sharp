09404cd1542b440c0805013f5c7fd06d79c80e3d
ssmall90 <ssmall9595@icloud.com>
2023-10-06 09:42:08 +0100

    Eye of Sauron Challenge Created and Completed

---
+++ Open Kattis/Eye of Sauron/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mynamespace
+{
+    class Myprogram
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string[] inputSplit = input.Split('(', ')');
+
+            if (inputSplit[0] == inputSplit[2])
+            {
+                Console.WriteLine("correct");
+            }
+            else
+            {
+                Console.WriteLine("fix");
+            }
+        }
+    }
+}
+
