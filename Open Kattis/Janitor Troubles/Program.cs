c1bd5aad45dcf32667bcf18a3686a46c9a5043f8
ssmall90 <ssmall9595@icloud.com>
2023-10-14 13:14:13 +0100

    "Janitor Troubles" Challenge Created and Completed

---
+++ Open Kattis/Janitor Troubles/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Mynamespace
+{
+    class MyProgram
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double a = double.Parse(input.Split(" ")[0]);
+            double b = double.Parse(input.Split(" ")[1]);
+            double c = double.Parse(input.Split(" ")[2]);
+            double d = double.Parse(input.Split(" ")[3]);
+
+            var MyMethod = new MyMethods();
+            double output = MyMethod.MaxArea(a, b, c, d);
+
+            Console.WriteLine(output.ToString());
+        }
+
+
+    }
+
+    public class MyMethods
+    {
+         public double MaxArea(double a, double b, double c, double d)
+        {
+            double semiPerimeter = SemiPerimeter(a, b, c, d);
+            return Formula(a, b, c, d, semiPerimeter);
+            
+        }
+
+        public double SemiPerimeter(double a, double b, double c, double d)
+        {
+            return (a + b + c + d) / 2;
+        }
+
+        public double Formula(double a, double b, double c, double d, double semiP)
+        {
+            return Math.Sqrt(
+                            (semiP - a) *
+                            (semiP - b) *
+                            (semiP - c) *
+                            (semiP - d)
+                            );
+        }
+    }
+
+}
