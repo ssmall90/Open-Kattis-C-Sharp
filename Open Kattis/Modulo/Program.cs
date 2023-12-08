8815a58300fdd6629841872e87e23da206622991
ssmall90 <ssmall9595@icloud.com>
2023-12-08 12:26:01 +0000

    Modulo Challenge Completed

---
+++ Open Kattis/Modulo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mynamespace
+{
+    class Myprogram
+    {
+        public static void Main(string[] args)
+        {
+            int currnetNumber = 0;
+            int value = 0;
+            List<int> numList = new List<int>();
+
+            int inputCounter = 10;
+
+            while(inputCounter > 0)
+            {
+                currnetNumber = int.Parse(Console.ReadLine());
+
+                value = currnetNumber % 42;
+
+                if (!numList.Contains(value))
+                {
+                    numList.Add(value);
+                }
+
+                inputCounter--;
+            }
+
+            Console.WriteLine(numList.Count);
+        }
+    }
+}
