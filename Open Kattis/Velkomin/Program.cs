d783aedf94dc12176540fc2ddc75b3605a165a88
ssmall90 <ssmall9595@icloud.com>
2023-12-08 11:52:43 +0000

    Velkomin Challenge Completed

---
+++ Open Kattis/Velkomin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    
+namespace Mynamespace
+{
+    class Myprogram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("VELKOMIN!");
+        }
+    }
+}
