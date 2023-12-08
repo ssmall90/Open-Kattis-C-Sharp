357b1ada8cd7f4368deb0cb771d6d191e6c33642
ssmall90 <ssmall9595@icloud.com>
2023-12-08 12:26:49 +0000

    Amerískur Vinnustaður Challenge Completed

---
+++ Open Kattis/Amerískur Vinnustaður/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mynamespace
+{
+    class Myprogram
+    {
+        public static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(input * 0.09144);
+        }
+    }
+}
