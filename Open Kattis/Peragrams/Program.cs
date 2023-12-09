6130daaa3fbb4b44fa269e579f4cdf29e743b578
ssmall90 <ssmall9595@icloud.com>
2023-12-09 12:05:50 +0000

    Peragrams Challenge Completed

---
+++ Open Kattis/Peragrams/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Mynamespace
+{
+    class Myprogram
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+
+            int count = 0;
+
+            foreach(char c in input)
+            {
+                if(!sb.ToString().Contains(c))
+                {
+                    sb.Append(c);
+                }
+            }
+
+            List<int> ints = new List<int>();
+            
+            foreach(char c in sb.ToString())
+            {
+                int numberOfOccurences = input.Where(letter => letter.Equals(c)).Count();
+                ints.Add(numberOfOccurences);
+
+            }
+
+            foreach(char num in ints)
+            {
+                if(num == 1)
+                {
+                    count++;
+                }
+                
+                if(num > 1 &&  num % 2 != 0)
+                {
+                    count++;
+                }
+
+            }
+
+            if(count == 0)
+            {
+                Console.WriteLine(count);
+            }
+            else
+            {
+                Console.WriteLine(count - 1);
+            }
+
+        }
+    }
+}
