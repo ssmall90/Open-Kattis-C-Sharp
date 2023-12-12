5d7ac7ac06a3a05547c876a69df7cdf5af3acfc8
ssmall90 <ssmall9595@icloud.com>
2023-12-12 14:50:47 +0000

    Simon Says Challenge Completed

---
+++ Open Kattis/Simon Says/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime;
+
+namespace Mynamespace
+{
+    class Myprogram
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            string[] responses = new string[numberOfLines];
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Split(" ")[0] == "Simon" && input.Split(" ")[1] == "says")
+                {
+
+                    responses[i] = input.Substring(11, input.Length - 11);
+                }
+
+            }
+            foreach (string response in responses)
+            {
+                if (response != "")
+                {
+                    Console.WriteLine(response);
+                }
+            }
+        }
+    }
+}
