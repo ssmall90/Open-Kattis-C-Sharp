ea4ecf0aa3640fb8c3ee4675950b7620df95b0b1
ssmall90 <ssmall9595@icloud.com>
2023-12-12 14:32:20 +0000

    Free Food Challenge Completed

---
+++ Open Kattis/Free Food/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mynamespace
+{
+    class Myprogram
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfEvents = int.Parse(Console.ReadLine());
+            List<int> inputs = new List<int>();
+
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+                string eventDates = Console.ReadLine();
+                inputs.Add(int.Parse(eventDates.Split(" ")[0]));
+                inputs.Add(int.Parse(eventDates.Split(" ")[1]));
+
+            }
+
+            int counter = 0;
+
+            List<int> duplicateDates = new List<int>();
+
+            for(int i = 0; i < inputs.Count; i = i +2)
+            {
+                
+                if (inputs[i] == inputs[i + 1] && !duplicateDates.Contains(inputs[i]))
+                {
+                    counter++;
+                    duplicateDates.Add(inputs[i]);
+                }
+                else
+                {
+
+                    for (int j = inputs[i]; j <= inputs[i + 1]; j++)
+                    {
+
+                        if (!duplicateDates.Contains(j))
+                        {
+                            counter++;
+                            duplicateDates.Add(j);
+                        }
+                    }
+                }
+
+            }
+
+                Console.WriteLine(counter); 
+        }
+    }
+}
